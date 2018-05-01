78b33e73650461b0681641079822620d588e8ef6
Athan Clark <athan.clark@localcooking.com>
2018-04-30 20:51:30 -0600

    user roles

---
+++ src/LocalCooking/Common/User/Role.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE
+    DeriveGeneric
+  , TemplateHaskell
+  , OverloadedStrings
+  #-}
+
+module LocalCooking.Common.User.Role where
+
+import Data.Aeson (FromJSON (..), ToJSON (..), Value (String))
+import Data.Aeson.Attoparsec (attoAeson)
+import Data.Attoparsec.Text (Parser, string)
+import Data.Hashable (Hashable)
+import Control.Applicative ((<|>))
+import Database.Persist.TH (derivePersistField)
+import GHC.Generics (Generic)
+
+
+data UserRole
+  = Customer
+  | Chef
+  | Farmer
+  | Editor
+  | Manager
+  deriving (Eq, Show, Read, Generic)
+derivePersistField "UserRole"
+
+instance Hashable UserRole
+
+instance ToJSON UserRole where
+  toJSON x = String $ case x of
+    Customer -> "customer"
+    Chef     -> "chef"
+    Farmer   -> "farmer"
+    Editor   -> "editor"
+    Manager  -> "manager"
+
+instance FromJSON UserRole where
+  parseJSON = attoAeson userRoleParser
+
+userRoleParser :: Parser UserRole
+userRoleParser = do
+  let customer = Customer <$ string "customer"
+      chef     = Chef <$ string "chef"
+      farmer   = Farmer <$ string "farmer"
+      editor   = Editor <$ string "editor"
+      manager  = Manager <$ string "manager"
+  customer <|> chef <|> farmer <|> editor <|> manager
