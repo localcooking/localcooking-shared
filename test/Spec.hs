16f19950775562d628d00560db202b7fe836f473
Athan Clark <athan.clark@gmail.com>
2018-05-25 11:17:45 -0600

    tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,21 +6,22 @@
 import LocalCooking.Common.AccessToken (AccessToken)
 import LocalCooking.Common.Diet (Diet)
 import LocalCooking.Common.Ingredient (Ingredient)
+import LocalCooking.Common.Order (OrderProgress)
 import LocalCooking.Common.Rating (Rating)
 import LocalCooking.Common.Tag (Tag)
-import LocalCooking.Common.Order (OrderProgress)
 import LocalCooking.Common.User.Name (Name)
 import LocalCooking.Common.User.Role (UserRole)
 import LocalCooking.Common.User.Password (HashedPassword)
 
-import LocalCooking.Semantic.Mitch (ReviewSynopsis, Review, MealSynopsis, Meal, ChefSynopsis, Chef, MenuSynopsis, Menu)
-import qualified LocalCooking.Semantic.Mitch as Mitch
-import LocalCooking.Semantic.Chef (MealSettings, ChefSettings, MenuSettings)
-import qualified LocalCooking.Semantic.Chef as Chef
-
+import Data.Address (USAAddress)
+import Data.Image.Source (ImageSource)
+import Data.List.Pagination (PaginationArgs)
+import Data.List.Sorting (SortingArgs)
+import Data.Price (Price)
 import Data.Text.Markdown (MarkdownText)
 import Data.Text.Permalink (Permalink)
-import Data.Image.Source (ImageSource)
+
+
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.QuickCheck (testProperty)
 
@@ -30,46 +31,50 @@ import qualified Data.Aeson as Aeson
 
 main :: IO ()
 main = defaultMain $ testGroup "JSON encodings"
-  [ testProperty "Data.Text.Markdown" (\(x :: MarkdownText) -> jsonIso x)
-  , testProperty "Data.Text.Permalink" (\(x :: Permalink) -> jsonIso x)
+  [ testProperty "Data.Address" (\(x :: USAAddress) -> jsonIso x)
   , testProperty "Data.Image.Source" (\(x :: ImageSource) -> jsonIso x)
+  , testProperty "Data.List.Pagination" (\(x :: PaginationArgs) -> jsonIso x)
+  , testProperty "Data.List.Sorting" (\(x :: SortingArgs ()) -> jsonIso x)
+  , testProperty "Data.Price" (\(x :: Price) -> jsonIso x)
+  , testProperty "Data.Text.Markdown" (\(x :: MarkdownText) -> jsonIso x)
+  , testProperty "Data.Text.Permalink" (\(x :: Permalink) -> jsonIso x)
   , testProperty "LocalCooking.Common.AccessToken" (\(x :: AccessToken) -> jsonIso x)
   , testProperty "LocalCooking.Common.Diet" (\(x :: Diet) -> jsonIso x)
   , testProperty "LocalCooking.Common.Ingredient" (\(x :: Ingredient) -> jsonIso x)
+  , testProperty "LocalCooking.Common.Order" (\(x :: OrderProgress) -> jsonIso x)
   , testProperty "LocalCooking.Common.Rating" (\(x :: Rating) -> jsonIso x)
   , testProperty "LocalCooking.Common.Tag" (\(x :: Tag) -> jsonIso x)
-  , testProperty "LocalCooking.Common.Order" (\(x :: OrderProgress) -> jsonIso x)
   , testProperty "LocalCooking.Common.User.Name" (\(x :: Name) -> jsonIso x)
   , testProperty "LocalCooking.Common.User.Role" (\(x :: UserRole) -> jsonIso x)
   , testProperty "LocalCooking.Common.User.Password" (\(x :: HashedPassword) -> jsonIso x)
 
-  , testProperty "LocalCooking.Semantic.Mitch.ReviewSynopsis"
-    (\(x :: ReviewSynopsis) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.Review"
-    (\(x :: Review) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.MealSynopsis"
-    (\(x :: MealSynopsis) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.Meal"
-    (\(x :: Meal) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.ChefSynopsis"
-    (\(x :: ChefSynopsis) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.Chef"
-    (\(x :: Chef) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.MenuSynopsis"
-    (\(x :: MenuSynopsis) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.Menu"
-    (\(x :: Menu) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Mitch.Order"
-    (\(x :: Mitch.Order) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.ReviewSynopsis"
+  --   (\(x :: ReviewSynopsis) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.Review"
+  --   (\(x :: Review) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.MealSynopsis"
+  --   (\(x :: MealSynopsis) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.Meal"
+  --   (\(x :: Meal) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.ChefSynopsis"
+  --   (\(x :: ChefSynopsis) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.Chef"
+  --   (\(x :: Chef) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.MenuSynopsis"
+  --   (\(x :: MenuSynopsis) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.Menu"
+  --   (\(x :: Menu) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Mitch.Order"
+  --   (\(x :: Mitch.Order) -> jsonIso x)
 
-  , testProperty "LocalCooking.Semantic.Chef.MealSettings"
-    (\(x :: MealSettings) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Chef.ChefSettings"
-    (\(x :: ChefSettings) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Chef.MenuSettings"
-    (\(x :: MenuSettings) -> jsonIso x)
-  , testProperty "LocalCooking.Semantic.Chef.Order"
-    (\(x :: Chef.Order) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Chef.MealSettings"
+  --   (\(x :: MealSettings) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Chef.ChefSettings"
+  --   (\(x :: ChefSettings) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Chef.MenuSettings"
+  --   (\(x :: MenuSettings) -> jsonIso x)
+  -- , testProperty "LocalCooking.Semantic.Chef.Order"
+  --   (\(x :: Chef.Order) -> jsonIso x)
   ]
 
 
