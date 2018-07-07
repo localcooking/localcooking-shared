f8d854c2a5876062915ee93903c2f9bebae18258
Athan Clark <athan.clark@gmail.com>
2018-07-07 09:19:44 -0600

    is string for markdown

--- src/Data/Text/Markdown.hs
+++ src/Data/Text/Markdown.hs
@@ -8,6 +8,7 @@ module Data.Text.Markdown where
 import Data.Text.Lazy (Text)
 import Data.Hashable (Hashable)
 import Data.Aeson (FromJSON, ToJSON)
+import Data.String (IsString)
 import Database.Persist.Class (PersistField)
 import Database.Persist.Sql (PersistFieldSql)
 import GHC.Generics (Generic)
@@ -17,7 +18,7 @@ import Test.QuickCheck.Instances ()
 
 newtype MarkdownText = MarkdownText
   { getMarkdownText :: Text
-  } deriving (Eq, Ord, Show, PersistField, PersistFieldSql, Hashable, Generic, ToJSON, FromJSON)
+  } deriving (Eq, Ord, Show, PersistField, PersistFieldSql, Hashable, Generic, ToJSON, FromJSON, IsString)
 
 instance Arbitrary MarkdownText where
   arbitrary = MarkdownText <$> arbitrary
